88343232c4aa8d6af18f83a81e60d473bf63af91
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-07-04 13:51:49 +0700

    test

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -77,7 +77,7 @@ def main(args):
                 nrof_images_total += 1
                 filename = os.path.splitext(os.path.split(image_path)[1])[0]
                 output_filename = os.path.join(output_class_dir, filename+'.png')
-                print(image_path)
+                #print(image_path)
                 if not os.path.exists(output_filename):
                     try:
                         img = misc.imread(image_path)
@@ -136,6 +136,7 @@ def main(args):
                             
     print('Total number of images: %d' % nrof_images_total)
     print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+    print('Percentage of success: %d %' % nrof_successfully_aligned/nrof_images_total)
             
 
 def parse_arguments(argv):
